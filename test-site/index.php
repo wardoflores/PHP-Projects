6955579e588d54c0d5f7a18bd9c09dfa8d49ac6f
wardoflores <eduardojoeyflores@gmail.com>
2023-09-13 03:35:05 +0800

    Create index.php

---
+++ test-site/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en-US">
+  <head>
+    <meta charset="utf-8" />
+    <title>My test page</title>
+    <meta name="author" content="Chris Mills" />
+	<meta
+	  name="description"
+	  content="The MDN Web Docs Learning Area aims to provide
+		complete beginners to the Web with all they need to know to get
+		started with developing websites and applications." />
+	<meta
+  http-equiv="Content-Security-Policy"
+	  content="default-src 'self'; img-src https://*; child-src 'none';" />
+	<link rel="icon" href="favicon.ico" type="image/x-icon" />
+	<link rel="stylesheet" href="my-css-file.css" />
+	<script src="my-js-file.js" defer></script>
+  </head>
+  <body>
+    <p>This is my page</p>
+  </body>
+</html>
